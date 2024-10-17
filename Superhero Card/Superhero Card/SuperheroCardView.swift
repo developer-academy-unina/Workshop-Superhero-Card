da0769db5129e65f85b2b7af18e3cfc71fa3e330
Tiago Pereira <tiagogomespereira@gmail.com>
2024-10-17 14:35:03 +0200

    Added Preview macro and replace foregroundColor

--- Superhero Card/Superhero Card/SuperheroCardView.swift
+++ Superhero Card/Superhero Card/SuperheroCardView.swift
@@ -3,6 +3,7 @@
 //  Superhero Card
 //
 //  Created by Pasquale Vittoriosi on 08/09/22.
+//  Updated by Tiago Pereira on 17/10/24.
 //
 
 import SwiftUI
@@ -17,74 +18,65 @@ struct SuperheroCardView: View {
          */
         
         VStack {
-            Group {
-                Text("Superhero Name")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.top, 30.0)
-                
-                /**
-                 Change the name of the image here.
-                 Remember to put the image in the *Assets* folder in the Project navigator panel on the left
-                 */
-                Image("Placeholder")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(40)
-                    .frame(width: 240, height: 240)
-                    .padding(.horizontal, 40)
-                
-                Text("Name Surname")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding()
-            }
+            Text("Superhero Name")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .padding(.top, 30.0)
             
-            Group {
-                Text("Powers:")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Text("Power 1, Power 2, Power 3, ..")
-                    .foregroundColor(.white)
-                    .padding(.horizontal)
-            }
+            /**
+             Change the name of the image here.
+             Remember to put the image in the *Assets* folder in the Project navigator panel on the left
+             */
+            Image("Placeholder")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(40)
+                .frame(width: 240, height: 240)
+                .padding(.horizontal, 40)
             
-            Group {
-                Text("Weaknesses:")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Text("Weakness 1, Weakness 2, ...")
-                    .foregroundColor(.white)
-                    .padding(.horizontal)
-            }
+            Text("Name Surname")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .padding()
             
-            Group {
-                Text("Famous for:")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                Text("Fun Fact here")
-                    .foregroundColor(.white)
-                    .padding([.horizontal, .bottom], 30.0)
-            }
+            Text("Powers:")
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Text("Power 1, Power 2, Power 3, ..")
+                .foregroundStyle(.white)
+                .padding(.horizontal)
+            
+            Text("Weaknesses:")
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Text("Weakness 1, Weakness 2, ...")
+                .foregroundStyle(.white)
+                .padding(.horizontal)
+            
+            Text("Famous for:")
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+            Text("Fun Fact here")
+                .foregroundStyle(.white)
+                .padding([.horizontal, .bottom], 30.0)
         }
         
         .background {
             RoundedRectangle(cornerRadius: 40)
-                .foregroundColor(.gray)
+                .foregroundStyle(.gray)
             RoundedRectangle(cornerRadius: 40)
                 .strokeBorder(.black, lineWidth: 10)
         }
+        
         .padding()
     }
 }
 
-struct SuperheroCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        SuperheroCardView()
-    }
+#Preview {
+    SuperheroCardView()
 }
